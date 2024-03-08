984436e01e777d5cc98b881d11a3d93df2ece0a6
Adithya Prasetio Hutahaean <145269520+HikaruYo@users.noreply.github.com>
2024-03-08 20:21:10 +0700

    Update Typed JS

--- js/script.js
+++ js/script.js
@@ -71,7 +71,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right'});
 
 // typed js
 const typed = new Typed('.multiple-text', {
-    strings: ['Web Developer', 'Data Scientist'],
+    strings: ['Web Developer', 'BackEnd Developer', 'Data Scientist'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
